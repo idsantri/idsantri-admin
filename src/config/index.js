4faddce5c226689adf07af095043ac94d26198d5
idsantri <idsantri.mail@gmail.com>
2024-12-02 13:53:43 +0700

    quasar config

--- src/config/index.js
+++ src/config/index.js
@@ -2,8 +2,11 @@ const config = {
 	INS_DESC: 'Madrasah &',
 	INS_NAME: 'Pondok Pesantren',
 	INS_SHORT: 'Demo Aplikasi',
-	BASE_API: 'https://demo.idsantri.my.id',
+	BASE_API: 'http://localhost:8000',
 	END_API: '/admin',
 	DEV: true,
+	PWA_NAME: 'ID Santri (Admin)',
+	PWA_SHORT_NAME: 'ID Santri',
+	PWA_DESCRIPTION: 'Aplikasi ID Santri (Admin)',
 };
 export default config;
