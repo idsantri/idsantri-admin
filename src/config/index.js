2e2a5da18b01dfaf7f35e5b04a3de537039ea273
idsantri <idsantri.mail@gmail.com>
2024-12-05 16:40:15 +0700

    config

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 const config = {
-	INS_DESC: 'Demo Aplikasi',
-	INS_NAME: 'Madrasah dan Pondok Pesantren',
-	INS_SHORT: 'Demo Aplikasi',
-	BASE_API: 'https://demo.idsantri.my.id',
+	INS_DESC: 'Demo Lokal',
+	INS_NAME: 'Pondok Pesantren',
+	INS_SHORT: 'Demo Aplikasi (lokal)',
+	BASE_API: 'http://localhost:8000',
 	END_API: '/admin',
 	DEV: true,
 	PWA_NAME: 'ID Santri (Admin)',
