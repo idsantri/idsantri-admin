8c29270a2a8c88071807619c74e3ba59379b163b
oets <oets.man@gmail.com>
2025-10-19 13:06:32 +0700

    carousel alamat

---
+++ src/components/inputs/InputSelectAlamat.vue
@@ -0,0 +1,64 @@
+<template lang="">
+	<q-select
+		dense
+		outlined
+		:label="label"
+		emit-value
+		map-options
+		v-model="input"
+		:options="options"
+		:loading="loading"
+		behavior="menu"
+		clearable
+	>
+		<template v-slot:after>
+			<q-card flat bordered>
+				<q-btn-dropdown
+					flat
+					dropdown-icon="more_vert"
+					class="q-px-sm"
+					color=""
+				>
+					<q-btn
+						no-caps
+						label="Reload"
+						flat
+						icon="sync"
+						v-close-popup
+						class="block"
+						@click="$emit('syncClick')"
+					/>
+					<q-separator />
+					<q-btn
+						no-caps
+						label="Setting"
+						flat
+						icon="toc"
+						to="/settings/alamat"
+						v-close-popup
+						class="block"
+						@click="$emit('routeClick')"
+					/>
+				</q-btn-dropdown>
+			</q-card>
+		</template>
+	</q-select>
+</template>
+<script setup>
+const input = defineModel();
+const props = defineProps({
+	options: {
+		type: Array,
+		default: () => [],
+	},
+	loading: {
+		type: Boolean,
+		default: false,
+	},
+	label: {
+		type: String,
+		default: () => 'Select Alamat',
+	},
+});
+</script>
+<style lang=""></style>
