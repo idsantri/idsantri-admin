5763b1abe0389af969ee3325d28b19a355bb2251
oets <oets.man@gmail.com>
2025-10-21 13:04:31 +0700

    add docs  to card loading

--- src/components/cards/CardLoading.vue
+++ src/components/cards/CardLoading.vue
@@ -1,6 +1,11 @@
 <template lang="">
-	<!-- inner loading must be on the end -->
 	<!-- <q-inner-loading :showing="showing" label="Please wait..." label-class="text-teal" label-style="font-size: 1.1em" /> -->
+
+	<!-- 
+		NOTE:
+		- parent must be relative position (or the relative-position CSS class declared)
+		- QInnerLoading must be the last element inside its parent so it can appear on top of the other content.
+	-->
 	<q-inner-loading :showing="showing">
 		<q-spinner-puff size="8em" color="green-14" />
 		<div class="text-subtitle2 text-green-10">Tunggu sebentar …</div>
