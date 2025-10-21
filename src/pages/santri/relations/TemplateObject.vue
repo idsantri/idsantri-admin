8f1fa2a2a29f653baaaad2c9bc468b293e7eb20a
oets <oets.man@gmail.com>
2025-10-21 20:05:25 +0700

    test card loading

--- src/pages/santri/relations/TemplateObject.vue
+++ src/pages/santri/relations/TemplateObject.vue
@@ -1,22 +1,20 @@
 <template>
-	<div>
-		<div v-if="props.spinner">
-			<q-spinner-cube color="green-12" size="8em" class="flex q-mx-auto" />
-		</div>
-		<div v-else style="min-height: 50px">
+	<div class="relative-position">
+		<div style="min-height: 50px">
 			<div class="row" v-for="(value, key) in props.data" :key="key">
-				<div class="col-4 text-caption">{{ key }}</div>
+				<div class="col-4 text-caption text-italic">{{ key }}</div>
 				<div class="col">{{ value }}</div>
 			</div>
 			<q-btn
 				icon="info"
 				color="green-8"
-				class="text-green-11 absolute-bottom-right q-ma-sm"
+				class="text-green-11 absolute-bottom-right"
 				:to="props.route"
 				round
 				glossy
 			/>
 		</div>
+		<CardLoading :showing="spinner" />
 	</div>
 </template>
 
