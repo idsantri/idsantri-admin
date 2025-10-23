3aa02c8cf16e6df11de7ca1d11f3088036b823fc
idsantri <idsantri.mail@gmail.com>
2025-10-23 23:14:55 +0700

    fix card header layout

--- src/components/cards/CardHeader.vue
+++ src/components/cards/CardHeader.vue
@@ -1,48 +1,53 @@
 <template lang="">
 	<q-card-section class="bg-green-8 text-green-11 q-pa-sm">
-		<div class="tw:flex tw:items-center tw:justify-between tw:gap-2">
+		<div class="tw:flex tw:flex-wrap tw:justify-between tw:items-center tw:gap-2 tw:w-full">
 			<!-- child 1 -->
-			<div class="text-subtitle2 tw:flex tw:items-center">
-				<q-btn
-					v-if="showReload"
-					icon="sync"
-					no-caps
-					@click="$emit('onReload')"
-					dense
-					flat
-					class="q-mr-xs"
-					title="Muat ulang"
-				/>
-				<img v-else src="/idsantri.svg" class="q-mr-xs" style="height: 24px" title="ID Santri" />
-				<div v-html="title"></div>
-			</div>
-			<!-- child 2 -->
-			<div class="tw:flex tw:items-center tw:gap-2 tw:justify-end">
-				<div class="tw:flex tw:items-center tw:gap-2">
-					<slot name="buttons"> </slot>
+			<div class="tw:flex-shrink-0">
+				<div class="tw:flex tw:items-center justify-start">
+					<q-btn
+						v-if="showReload"
+						icon="sync"
+						no-caps
+						@click="$emit('onReload')"
+						dense
+						flat
+						class="q-mr-xs"
+						title="Muat ulang"
+					/>
+					<img v-else src="/idsantri.svg" class="q-mr-xs" style="height: 24px" title="ID Santri" />
+					<div v-html="title" class="text-subtitle2"></div>
 				</div>
-				<q-btn
-					v-if="showBack"
-					no-caps
-					:label="$q.screen.lt.sm ? '' : 'Kembali'"
-					icon="reply"
-					dense
-					class="q-px-sm"
-					outline
-					@click="$router.go(-1)"
-					title="Kembali"
-				/>
-				<q-btn-dropdown
-					flat
-					no-caps
-					class="text-green-11"
-					dense
-					dropdown-icon="more_vert"
-					:disabled="!slotMore"
-					title="Lainnya"
-				>
-					<slot name="more"></slot>
-				</q-btn-dropdown>
+			</div>
+
+			<!-- child 2 -->
+			<div class="tw:text-right tw:ml-auto">
+				<div class="tw:flex tw:items-center tw:gap-2 tw:justify-end">
+					<div class="tw:flex tw:items-center tw:gap-2">
+						<slot name="buttons"> </slot>
+					</div>
+					<q-btn
+						v-if="showBack"
+						no-caps
+						:label="$q.screen.lt.sm ? '' : 'Kembali'"
+						icon="reply"
+						dense
+						class="q-px-sm"
+						outline
+						@click="$router.go(-1)"
+						title="Kembali"
+					/>
+					<q-btn-dropdown
+						flat
+						no-caps
+						class="text-green-11"
+						dense
+						dropdown-icon="more_vert"
+						:disabled="!slotMore"
+						title="Lainnya"
+					>
+						<slot name="more"></slot>
+					</q-btn-dropdown>
+				</div>
 			</div>
 		</div>
 	</q-card-section>
