3fd0c2233aae91271c4d43ba46c1471e824ee37a
idsantri <134589502+idsantri@users.noreply.github.com>
2025-11-24 12:50:17 +0000

    Deploying to gh-demangan from @ idsantri/idsantri-admin@c0f0dd08f4d6b964500dae58d48bc2ff23ff2b0c 🚀

---
+++ assets/axios-BuqZIe6f.js
@@ -0,0 +1,1 @@
+import{d as a}from"./index-CmH6mWX-.js";import{a as i}from"./index-C6XmgQ-a.js";import{a as r}from"./index-ZMixvj2o.js";const f=a(({app:o})=>{o.config.globalProperties.$axios=i,o.config.globalProperties.$api=r});export{r as api,f as default};
--- assets/loading-store-DVMpamVw.js
+++
@@ -1,1 +0,0 @@
-import{aA as i}from"./index-BKgLw7qS.js";const o=i("loading",{state:()=>({loadingMain:!1}),actions:{setLoadingMain(a){this.loadingMain=a}}});export{o as l};
