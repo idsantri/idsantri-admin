797c55f62b12e33dabf9345c8b681a493cc18729
oets <oets.man@gmail.com>
2025-11-27 11:09:33 +0700

    set config

--- src/config/index.js
+++ src/config/index.js
@@ -8,5 +8,25 @@ const config = {
 	PWA_NAME: 'Admin Syaichona.id',
 	PWA_SHORT_NAME: 'Admin Syaichona',
 	PWA_DESCRIPTION: 'Aplikasi Syaichona (Admin)',
+	MENU: {
+		sekretariat: {
+			alumni: true,
+		},
+		makhadiyah: {
+			mutasi: true,
+			quran: true,
+		},
+		keamanan: {
+			izin_pesantren: true,
+			indisipliner: true,
+		},
+		ugt: true,
+		setting: {
+			appWali: true,
+			virtualAccount: true,
+		},
+	},
 };
+const menu = config.MENU;
+export { menu };
 export default config;
