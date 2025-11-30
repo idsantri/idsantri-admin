d52930486af3caa371bd8c4a041a83fc4808126a
idsantri <134589502+idsantri@users.noreply.github.com>
2025-11-30 02:47:12 +0000

    Deploying to gh-syaichona from @ idsantri/idsantri-admin@3394d9ede3df7256c3f294419a68742e097b8b22 🚀

---
+++ assets/axios-DdWy5x56.js
@@ -0,0 +1,1 @@
+import{d as a}from"./index-CywAldKl.js";import{a as i}from"./index-DpzgZQa-.js";import{a as r}from"./index-BpCflRzf.js";const f=a(({app:o})=>{o.config.globalProperties.$axios=i,o.config.globalProperties.$api=r});export{r as api,f as default};
