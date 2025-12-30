e1a44c4c225eec7651d10998e5ba1827d91fe6e4
oets <oets.man@gmail.com>
2025-12-30 13:04:33 +0700

    filter select account

--- src/components/inputs/InputSelectAccount.vue
+++ src/components/inputs/InputSelectAccount.vue
@@ -1,55 +1,82 @@
 <template lang="">
-	<q-select
-		label="Pilih paket"
-		dense
-		outlined
-		emit-value
-		map-options
-		:options="optionsSelect"
-		:loading="loading"
-		behavior="menu"
-		option-value="id"
-		option-label="group_name"
-		clearable
-		v-model="input"
-		:rules="[(val) => !!val || 'Harus diisi!']"
-		:class="[$attrs.class]"
-		:hint="hintSelect"
-	>
-		<template v-slot:after>
-			<q-card flat bordered>
-				<q-btn-dropdown flat dropdown-icon="more_vert" class="q-px-sm" color="">
-					<q-btn
-						class="block full-width"
-						v-close-popup
-						no-caps
-						label="Muat ulang"
-						icon="sync"
-						flat
-						@click="reload"
-					/>
-					<q-separator />
-					<q-btn
-						class="block full-width"
-						v-close-popup
-						no-caps
-						label="Daftar akun"
-						icon="info"
-						flat
-						to="/apb/accounts"
-					/>
-				</q-btn-dropdown>
-			</q-card>
-		</template>
-	</q-select>
+	<div :class="[$attrs.class]">
+		<div class="tw:flex tw:gap-2 tw:items-center tw:justify-between q-mb-sm">
+			<q-radio
+				v-model="category"
+				checked-icon="task_alt"
+				unchecked-icon="panorama_fish_eye"
+				val="Aktiva"
+				label="Aktiva"
+				dense
+			/>
+			<q-radio
+				v-model="category"
+				checked-icon="task_alt"
+				unchecked-icon="panorama_fish_eye"
+				val="Pendapatan"
+				label="Pendapatan"
+				dense
+			/>
+			<q-radio
+				v-model="category"
+				checked-icon="task_alt"
+				unchecked-icon="panorama_fish_eye"
+				val="Biaya"
+				label="Biaya"
+				dense
+			/>
+		</div>
+		<q-select
+			label="Pilih Akun"
+			dense
+			outlined
+			emit-value
+			map-options
+			:options="options"
+			:loading="loading"
+			behavior="menu"
+			option-value="id"
+			option-label="group_name"
+			clearable
+			v-model="input"
+			:rules="[(val) => !!val || 'Harus diisi!']"
+			:hint="hintSelect"
+		>
+			<template v-slot:after>
+				<q-card flat bordered>
+					<q-btn-dropdown flat dropdown-icon="more_vert" class="q-px-sm" color="">
+						<q-btn
+							class="block full-width"
+							v-close-popup
+							no-caps
+							label="Muat ulang"
+							icon="sync"
+							flat
+							@click="reload"
+						/>
+						<q-separator />
+						<q-btn-dropdown
+							class="block full-width"
+							v-close-popup
+							no-caps
+							label="Daftar akun"
+							icon="info"
+							flat
+							to="/apb/accounts"
+						/>
+					</q-btn-dropdown>
+				</q-card>
+			</template>
+		</q-select>
+	</div>
 </template>
 <script setup>
 import { storeToRefs } from 'pinia';
 import accountsStore from 'src/stores/apb-accounts-store';
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 
 const input = defineModel();
-
+const category = ref('');
 const store = accountsStore();
 const { loading, accounts, optionsSelect } = storeToRefs(store);
 const hintSelect = computed(() => {
@@ -61,6 +88,15 @@ const hintSelect = computed(() => {
 	}
 });
 const reload = () => store.loadAll(false);
+const options = computed(() => {
+	if (category.value) {
+		return optionsSelect.value.filter(
+			(account) => account.category.toLowerCase() === category.value.toLocaleLowerCase(),
+		);
+	} else {
+		return optionsSelect.value;
+	}
+});
 onMounted(async () => {
 	if (!accounts.value?.length) {
 		await reload();
