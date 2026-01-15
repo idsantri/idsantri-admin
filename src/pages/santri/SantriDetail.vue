8e322ca4e783d8f903fa45920f4e97b5b97182db
oets <oets.man@gmail.com>
2026-01-15 11:21:36 +0700

    informasi umur santri

--- src/pages/santri/SantriDetail.vue
+++ src/pages/santri/SantriDetail.vue
@@ -65,7 +65,7 @@
 <script setup>
 import { ref, toRefs, onMounted, computed } from 'vue';
 import { useRoute } from 'vue-router';
-import { formatDateFull } from '../../utils/format-date';
+import { formatDateFull, getAge } from '../../utils/format-date';
 import CardIdentity from './CardIdentity.vue';
 import UploadImage from 'src/components/ImageUploader.vue';
 import santriStore from 'src/stores/santri-store';
@@ -98,6 +98,17 @@ async function loadImage() {
 	// console.log(img.image_url);
 	santri.image_url = img.image_url;
 }
+
+const umur = computed(() => {
+	if (!santri.value?.tgl_lahir) {
+		return '-';
+	} else {
+		const umur = getAge(santri.value.tgl_lahir);
+		return umur.tahun + ' tahun, ' + umur.bulan + ' bulan, ' + umur.hari + ' hari';
+	}
+
+	getAge(santri.value.tgl_lahir);
+});
 
 async function loadData() {
 	santriStore().$reset();
@@ -147,6 +158,7 @@ const identity = computed(() => ({
 		santri.value.kode_pos,
 	),
 	Kelahiran: `${santri.value.tmp_lahir || '-'}, ${formatDateFull(santri.value.tgl_lahir)}`,
+	Umur: umur.value,
 	'Data Akhir': santri.value.data_akhir || '-',
 }));
 
