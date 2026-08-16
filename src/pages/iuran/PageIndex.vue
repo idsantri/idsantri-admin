ef48ce40f1f3ab09595e7422cbe4799cbbd6ebf2
oets <oets.man@gmail.com>
2026-08-16 12:33:52 +0700

    feat(releases): add new release version 1.7.2 with features and update routing logic

--- src/pages/iuran/PageIndex.vue
+++ src/pages/iuran/PageIndex.vue
@@ -37,13 +37,19 @@
 						<q-item-section avatar>
 							<q-icon color="green" name="sym_o_list_alt_add" />
 						</q-item-section>
-						<q-item-section> Buat Tagihan </q-item-section>
+						<q-item-section>
+							<q-item-label>Buat Tagihan </q-item-label>
+							<q-item-label caption>Massal </q-item-label>
+						</q-item-section>
 					</q-item>
 					<q-item clickable v-close-popup to="/iuran/payments">
 						<q-item-section avatar>
 							<q-icon color="green" name="receipt_long" />
 						</q-item-section>
-						<q-item-section> Catatan Transaksi </q-item-section>
+						<q-item-section>
+							<q-item-label> Catatan Transaksi</q-item-label>
+							<q-item-label caption>via Payment Gateway</q-item-label>
+						</q-item-section>
 					</q-item>
 				</q-list>
 			</template>
